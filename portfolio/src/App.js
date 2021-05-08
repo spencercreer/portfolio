8ef02dd38395ac90f204469f50737ee9df75235f
spencercreer <spencercreer@gmail.com>
2021-05-07 19:38:21 -0600

    setup React portfolio file structure

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "./components/Container";
+import './App.css';
+
+function App() {
+  return (
+    <Container fluid>
+      <h1>New React Profile</h1>
+    </Container>
+  );
+}
+
+export default App;
