b1d606845dd30573437d7f99c212864c49bff031
spencercreer <spencercreer@gmail.com>
2021-05-07 22:12:09 -0600

    added contact form

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,9 +3,10 @@ import Nav from "./components/Nav"
 import Container from "./components/Container";
 import Intro from "./components/Intro";
 import Row from "./components/Row";
-import Col from  "./components/Col";
+import Col from "./components/Col";
 import Card from "./components/Card";
-import Footer from "./components/Footer"
+import Form from "./components/Form";
+import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
@@ -19,29 +20,32 @@ function App() {
         <Row>
           <Col size="md-3">
             <Card
-            src="./assets/img/switzerland.jpg"
-            alt="Spencer paragliding in the Swiss Alps."
+              src="./assets/img/switzerland.jpg"
+              alt="Spencer paragliding in the Swiss Alps."
             />
           </Col>
           <Col size="md-3">
             <Card
-            src="./assets/img/code.jpg"
-            alt="A blue screen with computer code."
+              src="./assets/img/code.jpg"
+              alt="A blue screen with computer code."
             />
           </Col>
           <Col size="md-3">
             <Card
-            src="./assets/img/military_satellite.jpg"
-            alt="Soldier with a laptop working on a satelitte antenna."
+              src="./assets/img/military_satellite.jpg"
+              alt="Soldier with a laptop working on a satelitte antenna."
             />
           </Col>
           <Col size="md-3">
             <Card
-            src="./assets/img/sweden.jpg"
-            alt="A small fishing pier with fishing boats on a dock"
+              src="./assets/img/sweden.jpg"
+              alt="A small fishing pier with fishing boats on a dock"
             />
           </Col>
         </Row>
+      </Container>
+      <Container>
+        <Form />
       </Container>
       <Footer />
     </div>
